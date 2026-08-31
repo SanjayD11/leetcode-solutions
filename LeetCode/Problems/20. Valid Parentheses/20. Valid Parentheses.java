5d6b291edbf5f7eca89abe25c01a51e1b37b657f
Sanjay D <sanjayraju5164@gmail.com>
2026-08-31 21:49:37 +0530

    [20] [Time Beats: 98.08%] [Memory Beats: 9.75%] - Leet2Hub

--- LeetCode/Problems/20. Valid Parentheses/20. Valid Parentheses.java
+++ LeetCode/Problems/20. Valid Parentheses/20. Valid Parentheses.java
@@ -1,37 +1,36 @@
 1class Solution {
 2    public boolean isValid(String s) {
 3        Stack <Character> st = new Stack <>();
-4        for (int i = 0; i< s.length();i++) {
-5            char c = s.charAt(i);
-6           
-7            if( (c=='('||c=='{'||c=='[')){
-8                st.push(c);
-9            }
-10            else if(c==')'){
-11                if(!st.isEmpty() && st.peek()=='('){
-12                    st.pop();
-13                }
-14                else 
-15                    return false;
-16            }
-17                else if(c=='}'){
-18                if(!st.isEmpty() && st.peek()=='{'){
-19                    st.pop();
-20                }
-21                else {
-22                    return false;
+4        for(char c : s.toCharArray()){
+5           
+6            if( (c=='('||c=='{'||c=='[')){
+7                st.push(c);
+8            }
+9            else if(c==')'){
+10                if(!st.isEmpty() && st.peek()=='('){
+11                    st.pop();
+12                }
+13                else 
+14                    return false;
+15            }
+16                else if(c=='}'){
+17                if(!st.isEmpty() && st.peek()=='{'){
+18                    st.pop();
+19                }
+20                else {
+21                    return false;
+22                }
 23                }
-24                }
-25            else if(c==']'){
-26                if(!st.isEmpty() && st.peek()=='['){
-27                    st.pop();
-28                }
-29                else 
-30                    return false;
-31            }
-32        }
-33       return st.isEmpty();
-34    }
-35}
-36
-37    
+24            else if(c==']'){
+25                if(!st.isEmpty() && st.peek()=='['){
+26                    st.pop();
+27                }
+28                else 
+29                    return false;
+30            }
+31        }
+32       return st.isEmpty();
+33    }
+34}
+35
+36    
