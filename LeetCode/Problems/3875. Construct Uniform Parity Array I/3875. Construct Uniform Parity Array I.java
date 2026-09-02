d06c5e8eaaa7af602baf939c093fbc0c7a415b8f
Sanjay D <sanjayraju5164@gmail.com>
2026-09-02 18:21:31 +0530

    [3875] [Time Beats: 100.00%] [Memory Beats: 94.09%] - Leet2Hub

---
+++ LeetCode/Problems/3875. Construct Uniform Parity Array I/3875. Construct Uniform Parity Array I.java
@@ -0,0 +1,11 @@
+1class Solution {
+2    public boolean uniformArray(int[] nums1) {
+3    return  true;    
+4    }
+5}
+6
+7// Answer is always true.
+8// Reason:
+9// At least one odd irundha → ellaa evens um (even - odd) use panni odd aakalam.
+10// Odd illa na, array already all even.
+11// So every valid input ku construction possible.
