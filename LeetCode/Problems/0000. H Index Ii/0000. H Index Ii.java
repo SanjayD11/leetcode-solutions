646e05c5c8d3929dd836a76e78170415ec1e3905
Sanjay D <sanjayraju5164@gmail.com>
2026-09-19 19:37:04 +0530

    [0000] [Time Beats: 100.00%] [Memory Beats: 67.00%] - Leet2Hub

--- LeetCode/Problems/0000. H Index Ii/0000. H Index Ii.java
+++ LeetCode/Problems/0000. H Index Ii/0000. H Index Ii.java
@@ -2,24 +2,21 @@
 2    public int hIndex(int[] citations) {
 3    int n = citations.length;
 4    int low = 0;
-5    int high = n;
+5    int high = n-1;
 6    int ans = 0;
 7    while(low <= high){
-8        int count = 0;
-9        int mid = low+(high-low)/2;
-10        for(int citation : citations){
-11            if(citation >= mid){
-12                count++;
-13            }
-14        }
-15        if(count >= mid){
-16            ans = mid;
-17            low = mid+1;
-18        }
-19        else{
-20            high = mid-1;
-21        }
-22    }    
-23    return ans;
-24    }
-25}
+8        int mid = low+(high-low)/2;
+9        int papers = n - mid;
+10        if(citations[mid] >= papers){
+11            ans = papers;
+12            high = mid-1;
+13        }
+14        else{
+15            low = mid+1;
+16        }
+17    }    
+18    return ans;
+19    }
+20}
+21
+22
